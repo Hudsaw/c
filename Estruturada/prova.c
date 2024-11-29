6dffc37f9d09054edd7dd8e8a5283099c26980d5
Hudsaw <hudsonrb@gmail.com>
2024-11-29 12:48:30 -0300

    47

--- Estruturada/prova.c
+++ Estruturada/prova.c
@@ -15,32 +15,14 @@ typedef struct {
 
 void encherTabela(Tabela *tabela) {
     strcpy(tabela->letras, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    tabela->numeros[0] = 1;
-    tabela->numeros[1] = 2;
-    tabela->numeros[2] = 3;
-    tabela->numeros[3] = 4;
-    tabela->numeros[4] = 5;
-    tabela->numeros[5] = 6;
-    tabela->numeros[6] = 7;
-    tabela->numeros[7] = 8;
-    tabela->numeros[8] = 9;
-    tabela->numeros[9] = 1;
-    tabela->numeros[10] = 2;
-    tabela->numeros[11] = 3;
-    tabela->numeros[12] = 4;
-    tabela->numeros[13] = 5;
-    tabela->numeros[14] = 6;
-    tabela->numeros[15] = 7;
-    tabela->numeros[16] = 8;
-    tabela->numeros[17] = 9;
-    tabela->numeros[18] = 1;
-    tabela->numeros[19] = 2;
-    tabela->numeros[20] = 3;
-    tabela->numeros[21] = 4;
-    tabela->numeros[22] = 5;
-    tabela->numeros[23] = 6;
-    tabela->numeros[24] = 7;
-    tabela->numeros[25] = 8;
+    int cont=1;
+    for (int i=0; i<26; i++){
+        tabela->numeros[i] = cont;
+        cont++;
+        if (cont==10){
+            cont = 1;
+        }
+    }
 }
 
 void incluirNome(Nome *nome, char *letras) {
