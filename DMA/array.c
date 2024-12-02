180f48eeb89d968f70cd6ca92cf8d19a4686d852
Hudsaw <hudsonrb@gmail.com>
2024-12-02 11:30:08 -0300

    82

--- DMA/array.c
+++ DMA/array.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 // #define SIZE 5
 
+
 void funcao1(int* arr, int asize){
     
     for (int i=0; i<asize; i++){
@@ -16,6 +17,19 @@ void funcao2(int* arr, int asize){
         printf("O valor#%d: %d\n", i+1, arr[i]);
     }
 }
+void funcao3(int** arr, int* asize){
+    int bsize;
+    printf("Digite o novo tamanho do array de %d elementos:", *asize);
+    scanf("%d", &bsize);
+    int* brr = (int*) realloc (*arr, bsize * sizeof(int));
+    if (brr == NULL) {
+        printf("Erro ao realocar a memória!\n");
+        return;
+    }
+    *arr = brr;
+    *asize = bsize;
+    printf("Memoria realocada com sucesso! Novo tamanho: %d elementos.\n", bsize);
+}
 
 void menu() {
     int escolha;
@@ -28,6 +42,8 @@ void menu() {
     printf("Digite o tamanho do array:");
     scanf("%d", &asize);
     grades = (int*) malloc (asize * sizeof(int));
+    // grades = (int*) calloc (asize, sizeof(int));
+    // Com o calloc inicializa a variável com o valor 0, cuidado ao usar!
 
     if(!grades){
         printf("Memoria nao alocada!");
@@ -35,12 +51,12 @@ void menu() {
         printf("Memoria alocada!");
     }
 
-
     do {
         printf("\nPrograma escrito para estudar!\n");
         printf("Escolha uma acao:\n");
         printf("1. Digite os numeros do array\n");
         printf("2. Mostre os numeros do array\n");
+        printf("3. Alterar o tamanho do array\n");
         printf("0. Sair\n");
         printf("Digite sua escolha: ");
         scanf("%d", &escolha);
@@ -51,6 +67,9 @@ void menu() {
             case 2:
                 funcao2(grades, asize);
                 break;
+            case 3:
+                funcao3(&grades, &asize);
+                break;
             case 0:
                 printf("Saindo...\n");
                 return;
@@ -59,8 +78,12 @@ void menu() {
                 break;
         }
     } while (escolha!=0);
+    
+    free(grades); //liberar a memoria antes de fechar o programa.
+
 }
 int main(void) {
     menu();
+    
     return 0;
 }
